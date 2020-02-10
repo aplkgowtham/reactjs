df45e33711403777d383e788cb50de0a151d7403
aplk0711 <gowthamkcse@gmail.com>
2020-02-10 17:27:43 +0530

    #initial commit

---
+++ es.js
@@ -0,0 +1,23 @@
+var a = [1, 2, 3, 4, 5]
+console.log(a.push(5))
+console.log("push", a)
+
+var a = [1, 2, 3, 4]
+console.log(a.pop())
+console.log("pop", a)
+
+var a = [1, 2, 3, 4, 5]
+console.log(a.unshift(5))
+console.log("unshift", a)
+
+var a = [1, 2, 3, 4]
+console.log(a.shift())
+console.log("shift", a)
+
+var a = [1, 2, 3, 4]
+console.log(a.slice(1))
+console.log("slice", a)
+
+var a = [1, 2, 3, 4]
+console.log(a.splice(0, 1))
+console.log("splice", a)
